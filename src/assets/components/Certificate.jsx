7bfe9140b73d88797703e6c9fab69f153f358f06
incotelanna23 <twispro.ananya@gmail.com>
2024-09-07 10:50:38 +0530

    updated-commit

--- src/assets/components/Certificate.jsx
+++ src/assets/components/Certificate.jsx
@@ -1,9 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useCallback, useRef } from "react";
+import { toPng } from "html-to-image";
 import "./Certificate.css";
 import certificateTemplate from "../certificate.png";
 const Certificate = () => {
+  const ref = useRef(null);
   const [name, setName] = useState("");
   const [course, setCourse] = useState("");
+  const onButtonClick = useCallback(() => {
+    if (ref.current === null) {
+      return;
+    }
+    toPng(ref.current, { cacheBust: true })
+      .then((dataUrl) => {
+        const link = document.createElement("a");
+        link.download = `${name || "default"}-certificate.png`;
+        link.href = dataUrl;
+        link.click();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [ref]);
+
   return (
     <div>
       <div style={{ marginBottom: "10px" }}>
@@ -26,13 +44,14 @@ const Certificate = () => {
           onChange={(e) => setCourse(e.target.value)}
         />
       </div>
-      <div className="container">
+      <div className="container" ref={ref}>
         <img src={certificateTemplate} alt="certificate" height={400} />
         <div className="content">
           <h1 className="name">{name}</h1>
           <p className="course">{course}</p>
         </div>
       </div>
+      <button onClick={onButtonClick}>Click Me</button>
     </div>
   );
 };
